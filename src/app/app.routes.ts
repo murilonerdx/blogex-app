e8c9297a8eecab4b7be5dd1a821cc74b22e2fa17
prinln <mu-silva@outlook.com>
2024-03-27 14:02:17 -0300

    Adicionando projeto inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from "@angular/core";
+import {HeaderComponent} from "./components/header/header.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
